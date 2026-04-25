ce039540db410ac9ef128f0fb4783b76dd8e2abe
DriftWare07 <ikaikaswearingen@gmail.com>
2026-04-25 13:29:55 -0500

    javascript updated

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ function $(id){
 }
 
 let clarice = $("clarice")
+let time = $("timer")
+
+let currentDate = new Date()
+
+let nextDate = new Date()
+
+
+
+
+
+console.log(nextDate)
 
 function makeSick(){
     clarice.classList.remove("bounce")
@@ -17,3 +28,43 @@ function makeHealthy(){
     clarice.src = "./assets/clarice.png"
 }
 
+
+
+
+
+function getNextDate(){
+    nextDate.setDate(currentDate.getDate()+1)
+    
+    //nextDate.setHours(currentDate.hours)
+}
+
+function updateTimer(){
+    currentDate = new Date();
+
+    
+
+    time.textContent = getTimeLeft()
+    
+
+
+}
+
+function getTimeLeft(timeout) {
+    return msToTime(new Date() - nextDate)
+}
+
+function msToTime(duration) {
+  var milliseconds = Math.floor((duration % 1000) / 100)*-1,
+    seconds = Math.floor((duration / 1000) % 60)*-1,
+    minutes = Math.floor((duration / (1000 * 60)) % 60)*-1,
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)*-1;
+
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+  return hours + ":" + minutes + ":" + seconds;
+}
+
+getNextDate()
+setInterval(updateTimer, 1)
